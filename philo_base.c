02f9e4fbbc6a65c1aa46bf7fd9bde7c5e16be6c6
Alessandro Metta <ametta@e3r5p3.42roma.it>
2021-09-15 12:46:21 +0200

    some improvement

--- philo_base.c
+++ philo_base.c
@@ -2,56 +2,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
 
-pthread_mutex_t m_forks[2];
-pthread_mutex_t m_write;
+pthread_mutex_t	m_write;
+pthread_mutex_t	*m_forks;
+pthread_mutex_t	*m_someboy_died;
+int				no_philo;//number of philosophers and forks
 
-void *routine(void *nu)
+uint64_t getTime()
 {
-	while (1)
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * (uint64_t)1000) + (tv.tv_usec / 1000));
+}
+
+void message(int nu, char *msg)
+{
+	pthread_mutex_lock(&m_write);
+	printf("[%llu]\tphilo %d %s\n", getTime(), nu, msg);
+	pthread_mutex_unlock(&m_write);
+}
+
+void *routine(void *arg)
+{
+	int i = 0;
+	int nu = *((int *) arg);
+	while (i < 5)
 	{
-	pthread_mutex_lock(&m_forks[0]);
-	pthread_mutex_lock(&m_write);
-	printf("philo %d take a fork\n", (int) nu);
-	pthread_mutex_unlock(&m_write);
-	pthread_mutex_lock(&m_forks[1]);
-	pthread_mutex_lock(&m_write);
-	printf("philo %d take a fork\n", (int) nu);
-	printf("philo %d is eating...\n", (int) nu);
-	pthread_mutex_unlock(&m_write);
-	usleep(100);
-	pthread_mutex_lock(&m_write);
-	printf("philo %d eaten\n", (int) nu);
-	pthread_mutex_unlock(&m_write);
-	pthread_mutex_unlock(&m_forks[0]);
-	pthread_mutex_unlock(&m_forks[1]);
-	pthread_mutex_lock(&m_write);
-	printf("philo %d is sleeping...\n", (int) nu);
-	pthread_mutex_unlock(&m_write);
-	usleep(100);
-	pthread_mutex_lock(&m_write);
-	printf("philo %d thinking\n", (int) nu);
-	pthread_mutex_unlock(&m_write);
-	usleep(100);
+		i++;
+		pthread_mutex_lock(&m_forks[nu]);
+		message(nu, "take a fork");
+		pthread_mutex_lock(&m_forks[(nu + i) % no_philo]);
+		message(nu, "take a fork");
+		message(nu, "is eating...");
+		usleep(100);
+		message(nu, "has finished eating");
+		pthread_mutex_unlock(&m_forks[nu]);
+		pthread_mutex_unlock(&m_forks[(nu + i) % no_philo]);
+		message(nu, "is sleeping...");
+		usleep(100);
+		message(nu, "is thinking");
+		usleep(100);
 	}
 	return (NULL);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	pthread_t philo1, philo2;
+	pthread_t	*philo;
+	int			i;
 
-	pthread_mutex_init(&m_forks[0], NULL);
-	pthread_mutex_init(&m_forks[1], NULL);
+	i = 0;
+	(void)argc;
+
+	// taking and converting the arguments
+	no_philo = atoi(argv[1]);
+
+	// mallocing the thread and the mutex
+	philo = malloc(sizeof(pthread_t) * no_philo);
+	m_forks = malloc(sizeof(pthread_mutex_t) * no_philo);
+	if (!philo)
+		return (1);
+	if (!m_forks)
+		return (1);
+
+	// initialization of forks(mutex)
+	i = 0;
+	while (i < no_philo)
+	{
+		pthread_mutex_init(&m_forks[i], NULL);
+		i++;
+	}
+
+	// initialization of write mutex
 	pthread_mutex_init(&m_write, NULL);
+	// pthread_mutex_init(&m_somebody_died, NULL);
 
-	pthread_create(&philo1, NULL, routine, (void *)1);
-	usleep(100);
-	pthread_create(&philo2, NULL, routine, (void *)2);
-	pthread_join(philo1, NULL);
-	pthread_join(philo2, NULL);
-	pthread_mutex_destroy(&m_forks[0]);
-	pthread_mutex_destroy(&m_forks[1]);
+	// creation of philosophers threads
+	i = 1;
+	while (i <= no_philo)
+	{
+		pthread_create(&philo[i], NULL, routine, (void *)&i);
+		pthread_detach(philo[i]);
+		usleep(100);
+		i++;
+	}
+
+	// closing the program
+	
+	sleep(5);
+	i = 0;
+	while (i < no_philo)
+	{
+		pthread_mutex_destroy(&m_forks[i]);
+		i++;
+	}
 	pthread_mutex_destroy(&m_write);
+	free(philo);
 	return (0);
 }
